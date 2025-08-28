7e3bdc506674ce0272eb885dfd44506b13db56a4
Ilia Koren <ilia.k@maddevs.io>
2025-08-28 22:29:21 +0300

    1-start added a separate getUserInput func

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -10,3 +10,14 @@ func main() {
 
 	fmt.Print(EUROToRUB)
 }
+
+func getUserInput() string {
+	var input string
+	fmt.Print("Enter your input: ")
+	fmt.Scan(&input)
+	return input
+}
+
+func calculate(currencyFrom, currencyTo string) float64 {
+	return 1.0
+}
